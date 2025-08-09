d55916f879e6487a57825cebb1ebaa7347b0d9d8
Aum112233 <asflimdy@gmail.com>
2025-08-09 21:41:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function playMusic() {
+  const music = document.getElementById("bgm");
+  music.play();
+
+  // สร้างหัวใจลอย
+  setInterval(() => {
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = Math.random() * 5 + 5 + "s";
+    document.body.appendChild(heart);
+    setTimeout(() => heart.remove(), 10000);
+  }, 300);
+}
